53f01417c507ea2cdd2f267a2fb1e671c782805c
little9 <jamie@jamielittle.org>
2023-01-17 11:09:51 -0600

    Add item metadata requests to main component

--- .ng/src/app/main/main.component.ts
+++ .ng/src/app/main/main.component.ts
@@ -1,5 +1,5 @@
-import { Observable  } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
+import { Observable, of  } from 'rxjs';
+import { filter, finalize, switchMap, tap } from 'rxjs/operators';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CloudAppRestService, CloudAppEventsService, Request, HttpMethod, 
   Entity, RestErrorResponse, AlertService } from '@exlibris/exl-cloudapp-angular-lib';
@@ -13,11 +13,12 @@ import { MatRadioChange } from '@angular/material/radio';
 export class MainComponent implements OnInit, OnDestroy {
 
   loading = false;
-  selectedEntity: Entity;
   apiResult: any;
-
+  
   entities$: Observable<Entity[]> = this.eventsService.entities$
-  .pipe(tap(() => this.clear()))
+  entitiesMetadata$: Observable<Entity[]>;
+  items$: Observable<any>;
+  items: Array<any> = [];
 
   constructor(
     private restService: CloudAppRestService,
@@ -25,60 +26,41 @@ export class MainComponent implements OnInit, OnDestroy {
     private alert: AlertService 
   ) { }
 
+
   ngOnInit() {
+    this.entities$.pipe(filter(entities => entities.length > 0), 
+    switchMap(entities => entities),  // switch to entity
+    filter(entities => entities.type === "BIB_MMS")).subscribe({ // filter to bibs
+      next: (entity) => { 
+        this.restService.call(entity.link).pipe(switchMap(response => { return of(response) })).subscribe({ 
+          next: (response) => {
+            this.restService.call(response.holdings.link).pipe(switchMap(response => { return of(response.holding) })).subscribe({
+              next: (response) => {
+                console.log(response)
+                 this.restService.call(response[0].link + "/items").pipe(switchMap(response => { return of(response) })).subscribe({
+                  next: (response) => {
+                    if(response.item) {
+                      this.items.push(response.item[0].item_data)
+                    }
+                    
+                  },
+                  error: (error) => {
+                    console.log(error)
+                  }
+                })
+              }
+            })
+          }
+        })
+      }
+    })
+
+
   }
 
   ngOnDestroy(): void {
   }
 
-  entitySelected(event: MatRadioChange) {
-    const value = event.value as Entity;
-    this.loading = true;
-    this.restService.call<any>(value.link)
-    .pipe(finalize(()=>this.loading=false))
-    .subscribe(
-      result => this.apiResult = result,
-      error => this.alert.error('Failed to retrieve entity: ' + error.message)
-    );
-  }
 
-  clear() {
-    this.apiResult = null;
-    this.selectedEntity = null;
-  }
 
-  update(value: any) {
-    const requestBody = this.tryParseJson(value)
-    if (!requestBody) return this.alert.error('Failed to parse json');
-
-    this.loading = true;
-    let request: Request = {
-      url: this.selectedEntity.link, 
-      method: HttpMethod.PUT,
-      requestBody
-    };
-    this.restService.call(request)
-    .pipe(finalize(()=>this.loading=false))
-    .subscribe({
-      next: result => {
-        this.apiResult = result;
-        this.eventsService.refreshPage().subscribe(
-          ()=>this.alert.success('Success!')
-        );
-      },
-      error: (e: RestErrorResponse) => {
-        this.alert.error('Failed to update data: ' + e.message);
-        console.error(e);
-      }
-    });    
-  }
-
-  private tryParseJson(value: any) {
-    try {
-      return JSON.parse(value);
-    } catch (e) {
-      console.error(e);
-    }
-    return undefined;
-  }
 }
