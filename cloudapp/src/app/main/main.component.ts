2804befb5b01fd62e8f3b3ed29a61751a87155e7
Jamie Little <jamie@jamielittle.org>
2023-01-30 15:15:41 -0600

    Basic RMST number display on a search result page (#2)
    * Add print button component
    
    * Add item metadata requests to main component
    
    * Display RMST numbers for current search
    
    This should show the RMST numbers for items when you open the cloud
    app on a search result page that has physical items/items with RMST
    numbers.
    
    Most of this code is the requests needed to get to the item level
    metadata.
    
    * Move requests to a service
    
    * Add sorting and basic page styles

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -1,9 +1,10 @@
-import { Observable  } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CloudAppRestService, CloudAppEventsService, Request, HttpMethod, 
-  Entity, RestErrorResponse, AlertService } from '@exlibris/exl-cloudapp-angular-lib';
-import { MatRadioChange } from '@angular/material/radio';
+import {
+  CloudAppRestService, CloudAppEventsService, Entity, AlertService
+} from '@exlibris/exl-cloudapp-angular-lib';
+import { Item } from '../models/item.model';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-main',
@@ -13,72 +14,33 @@ import { MatRadioChange } from '@angular/material/radio';
 export class MainComponent implements OnInit, OnDestroy {
 
   loading = false;
-  selectedEntity: Entity;
   apiResult: any;
 
   entities$: Observable<Entity[]> = this.eventsService.entities$
-  .pipe(tap(() => this.clear()))
+
+  items: Array<Item> = [];
 
   constructor(
     private restService: CloudAppRestService,
     private eventsService: CloudAppEventsService,
-    private alert: AlertService 
+    private alert: AlertService,
+    private itemService: ItemService
   ) { }
 
+
   ngOnInit() {
+    this.itemService = new ItemService(this.restService, this.eventsService);
+    this.itemService.getItems().subscribe({
+      next: (item) => { this.items.push(item as Item); this.itemService.sortItems(this.items) },
+      error: (err) => { this.alert.error(err) }
+    })
+
+
   }
 
   ngOnDestroy(): void {
   }
 
-  entitySelected(event: MatRadioChange) {
-    const value = event.value as Entity;
-    this.loading = true;
-    this.restService.call<any>(value.link)
-    .pipe(finalize(()=>this.loading=false))
-    .subscribe(
-      result => this.apiResult = result,
-      error => this.alert.error('Failed to retrieve entity: ' + error.message)
-    );
-  }
 
-  clear() {
-    this.apiResult = null;
-    this.selectedEntity = null;
-  }
 
-  update(value: any) {
-    const requestBody = this.tryParseJson(value)
-    if (!requestBody) return this.alert.error('Failed to parse json');
-
-    this.loading = true;
-    let request: Request = {
-      url: this.selectedEntity.link, 
-      method: HttpMethod.PUT,
-      requestBody
-    };
-    this.restService.call(request)
-    .pipe(finalize(()=>this.loading=false))
-    .subscribe({
-      next: result => {
-        this.apiResult = result;
-        this.eventsService.refreshPage().subscribe(
-          ()=>this.alert.success('Success!')
-        );
-      },
-      error: (e: RestErrorResponse) => {
-        this.alert.error('Failed to update data: ' + e.message);
-        console.error(e);
-      }
-    });    
-  }
-
-  private tryParseJson(value: any) {
-    try {
-      return JSON.parse(value);
-    } catch (e) {
-      console.error(e);
-    }
-    return undefined;
-  }
 }
