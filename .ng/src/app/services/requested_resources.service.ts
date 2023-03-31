549477887ac1abb260252b8f2317a464d901aab5
little9 <jamie@jamielittle.org>
2023-03-31 12:43:25 -0500

    Major refactor 💥
    This is a major refactor that gets the pick list directly from the
    
    `/almaws/v1/task-lists/requested-resources` API.
    
    It can now be activated anywhere in Alma. Before submitting this for review
    
    we will need to create a way to change the library
    and circ desk location.

---
+++ .ng/src/app/services/requested_resources.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { CloudAppRestService } from '@exlibris/exl-cloudapp-angular-lib';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { finalize, tap, catchError } from 'rxjs/operators';
+import { RequestedResources } from '../interfaces/requested-resources.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestedResourcesService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  
+  constructor(
+    private restService: CloudAppRestService,
+  ) { }
+
+  getRequestedResources(library: string, circ_desk: string, limit: number, alert: any): Observable<RequestedResources> {
+    const apiUrl = `/almaws/v1/task-lists/requested-resources?library=${library}&circ_desk=${circ_desk}&limit=${limit.toString()}`; 
+    this.loadingSubject.next(true);
+
+    return this.restService.call<RequestedResources>(apiUrl).pipe(
+      tap(result => console.log('result', result)),
+      finalize(() => this.loadingSubject.next(false)),
+      catchError(error => {
+        this.loadingSubject.next(false);
+        alert.error('Failed to retrieve entity: ' + error.message);
+        throw error;
+      })
+    );
+  }
+
+  isLoading(): Observable<boolean> {
+    return this.loadingSubject.asObservable();
+  }
+}
--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -1,11 +1,7 @@
-import { Observable, Subject, Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {
-  CloudAppRestService, CloudAppEventsService, Entity, AlertService
-} from '@exlibris/exl-cloudapp-angular-lib';
-import { Item } from '../interfaces/item.model';
-import { ItemService } from '../item.service';
-import { takeUntil } from 'rxjs/operators';
+import { AlertService } from '@exlibris/exl-cloudapp-angular-lib';
+import { RequestedResource, RequestedResources } from '../interfaces/requested-resources.interface';
+import { RequestedResourcesService } from '../services/requested_resources.service';
 
 @Component({
   selector: 'app-main',
@@ -14,77 +10,51 @@ import { takeUntil } from 'rxjs/operators';
 })
 
 export class MainComponent implements OnInit, OnDestroy {
-  apiResult: any;
-  entities$: Observable<Entity[]> = this.eventsService.entities$;
-  items: Array<Item> = [];
-  itemCount = 0;
-  
-  selectOptions = [
-    { value: '', label: 'Select an option' },
-    { value: 'all', label: 'Show All' },
-    { value: 'noblank', label: 'Hide Blank RMSTs' },
-  ];
-
-  selectedOption = this.selectOptions[0].value;
   loading: boolean;
   noItemsMessage: string;
-  
-  selectOption(event: Event) {
-    this.items.forEach(item => {
-      if (this.selectedOption == 'all') {
-        item.item_data.hidden = false;
-      } else if (this.selectedOption == 'noblank') {
-        if (item.item_data.storage_location_id == '' || item.item_data.storage_location_id == 'roo') {
-          item.item_data.hidden = true;
-        } else {
-          item.item_data.hidden = false;
-        }
-      }
-    });
-
-    this.itemCount = this.items.filter(item => item.item_data.hidden == false).length;
-  }
   today: number = Date.now();
+  requestedResources: RequestedResources;
+  rmst_checked: boolean = false;
+  requester_checked: boolean = false;
+  
+  library: string = 'MAIN';
+  circ_desk: string = 'DEFAULT_CIRC_DESK';
 
   constructor(
-    private restService: CloudAppRestService,
-    private eventsService: CloudAppEventsService,
     private alert: AlertService,
-    private itemService: ItemService
+    private requestedResourcesService: RequestedResourcesService,
   ) { }
 
-  removeItem(item: Item) {
-    this.items = this.items.filter(i => i !== item);
+  ngOnInit() {
+    this.requestedResourcesService.isLoading().subscribe(
+      loading => this.loading = loading
+    );
+
+    this.requestedResourcesService.getRequestedResources(this.library, this.circ_desk, 100, this.alert).subscribe(
+      result => {
+        this.requestedResources = result
+      }
+    );
   }
 
-  ngOnInit() {
-    this.items = [];
-    this.itemService = new ItemService(this.restService, this.eventsService);
-    this.loading = true
-    this.itemService.getItems().subscribe({
-      next: (item) => { 
-        this.items.push(item as Item);
-        this.itemService.sortItems(this.items);
-        this.itemCount = this.items.length;
-        this.items.forEach(item => item.item_data.hidden = false);
-        this.loading = this.itemService.loading;
-      },
-      complete: () => {
-        this.loading = this.itemService.loading;
-      },
-      error: (err) => { 
-        this.loading = false;
-        this.alert.error(err);  
-      },
-    })
+  getVisibleResources(): RequestedResource[] {
+    return this.requestedResources.requested_resource.filter(resource =>
+      resource.location.copy[0] && resource.location.copy[0].storage_location_id != '' || this.rmst_checked || this.requester_checked
+    );
+  }
 
+  getTotalVisible(): number {
+    return this.getVisibleResources().length;
+  }
+  
+  hideResource(index: number): void {
+    const filteredAndSortedResources = this.getVisibleResources();
+    if (filteredAndSortedResources[index]) {
+      const resourceId = filteredAndSortedResources[index].location.holding_id.value;
+      this.requestedResources.requested_resource = this.requestedResources.requested_resource.filter(resource => resource.location.holding_id.value !== resourceId);
+    }
   }
 
   ngOnDestroy(): void {
-   this.items = [];
-
   }
-
-
-
 }
